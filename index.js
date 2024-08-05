58441202fd00d4c5290592d05839daa198e617ee
Isbeli Ramirez <isbeliramirez8@gmail.com>
2024-08-05 18:00:46 -0400

    perf: Adding a code change that improves performance

---
+++ index.js
@@ -0,0 +1,10 @@
+const pessoa = {
+  nome: nome = prompt("Seu Nome:"),
+  sobreNome: sobreNome = prompt("Seu Sobrenome:"),
+  idade: idade = parseInt(prompt("Qual é sua idade:")),
+  corFavorito: corFavorito = prompt("Cor favorito:"),
+}
+
+Object
+console.log(`Olá sou  ${this.nome} ${this.sobreNome} e tenho ${this.idade} anos e minha cor favorito é ${this.corFavorito}`);
+console.table(pessoa);
